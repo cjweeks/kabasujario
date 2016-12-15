99eb3e23825ff14cf581d6e966298c6fb57d7e2e
Connor Weeks <cweeks314@gmail.com>
2016-12-14 23:20:19 -0500

    Edited second build script

--- build-heroku.sh
+++ build-heroku.sh
@@ -3,3 +3,5 @@
 node_modules/.bin/babel --source-maps --out-file public/camera-compiled.js --presets es2015 public/camera.js
 node_modules/.bin/babel --source-maps --out-file public/client-compiled.js --presets es2015 public/client.js
 node_modules/.bin/babel --source-maps --out-file public/game-compiled.js --presets es2015 public/game.js
+echo everything
+ls
